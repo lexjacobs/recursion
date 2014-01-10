25e9282bdb6f763d2eea3b844bc6abc45df58340
Alex Jacobs <lex.jacobs@gmail.com>
2014-01-09 23:47:15 -0800

    Hacking away... currently serving all types, except {Objects}

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,6 +2,43 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to have to write it from scratch:
-var stringifyJSON = function (obj) {
-  // your code goes here
+var stringifyJSON = function(obj) {
+  if (obj === null) {
+    return '' + obj;
+  }
+  if (typeof obj === 'string') {
+    return '\"' + obj + '\"';
+  }
+  if (typeof obj === 'number') {
+    return '' + obj;
+  }
+  if (typeof obj === 'boolean') {
+    return '' + obj;
+  }
+  if (Array.isArray(obj) === true && obj.length !== 0) {
+    var newStr = [];
+    for (var i = 0; i < obj.length; i++) {
+      if (typeof obj[i] === 'number') {
+        newStr.push(obj[i]);
+      } else {
+        newStr.push(stringifyJSON(obj[i]));
+      }
+    }
+    newStr2 = ["["];
+    newStr2.push(newStr);
+    newStr2.push("]");
+    //newStr.push("'");
+    //newStr.unshift("'");
+    //newStr = newStr.join("'");
+    return newStr2.join("");
+  }
+  if (Array.isArray(obj) === true && obj.length === 0) {
+    var newStr = ["[", "]"];
+    return newStr.join("");
+  }
+  if (typeof obj === 'object' && Object.keys(obj).length === 0) {
+    var newStr = ["{", "}"];
+    return newStr.join("");
+  }
+
 };
