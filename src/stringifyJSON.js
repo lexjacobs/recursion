a1b552f8ed2cda311fdf11cafc5440142c65c16f
Alex Jacobs <lex.jacobs@gmail.com>
2014-01-11 22:57:10 -0800

    In progress, but another fruitful waypoint on the road to completion.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,64 +3,69 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function(obj) {
-  if (obj === null) {
-    return '' + obj;
-  }
-  if (typeof obj === 'string') {
-    return '\"' + obj + '\"';
-  }
-  if (typeof obj === 'number') {
-    return '' + obj;
-  }
-  if (typeof obj === 'boolean') {
-    return '' + obj;
-  }
-  if (Array.isArray(obj) === true && obj.length !== 0) {
-    var newStr = [];
-    for (var i = 0; i < obj.length; i++) {
-      if (typeof obj[i] === 'number') {
-        newStr.push(obj[i]);
-      } else {
-        newStr.push(stringifyJSON(obj[i]));
-      }
-    }
-    newStr2 = ["["];
-    newStr2.push(newStr);
-    newStr2.push("]");
-    return newStr2.join("");
-  }
-  if (Array.isArray(obj) === true && obj.length === 0) {
-    var newStr = ["[", "]"];
-    return newStr.join("");
-  }
-  if (typeof obj === 'object' && Object.keys(obj).length === 0) {
-    var newStr = ["{", "}"];
-    return newStr.join("");
-  }
-  if (typeof obj === 'object' && Object.keys(obj).length !== 0) {
+  if (obj !== null && Array.isArray(obj) === false && typeof obj === 'object' && Object.keys(obj).length !== 0) {
     var newStr = ["{"];
     var oKobj = Object.keys(obj);
+
     for (var i = 0; i < oKobj.length; i++) {
-      if (typeof oKobj[i] === 'boolean') {
+      if (oKobj[i] === undefined || typeof oKobj[i] === 'function' || obj[oKobj[i]] === undefined || typeof obj[oKobj[i]] === 'function') {
+        continue;
+      } else {
+        newStr.push('\"');
         newStr.push(oKobj[i]);
+        newStr.push('\"');
       }
-      if (typeof oKobj[i] === 'number' || typeof oKobj[i] === 'string') {
+      newStr.push(":");
+      if (Array.isArray(obj[oKobj[i]]) === true) {
+        stringifyJSON(obj[oKobj[i]]);
+      }
+      if (typeof obj[oKobj[i]] === 'number') {
+        newStr.push(obj[oKobj[i]]);
+      }
+      if (typeof obj[oKobj[i]] === 'boolean') {
+        newStr.push(obj[oKobj[i]]);
+      } else {
         newStr.push('\"');
-        newStr.push(oKobj[i]);
-        newStr.push('\"');
-        newStr.push(":");
-        newStr.push('\"');  
         newStr.push(obj[oKobj[i]]);
         newStr.push('\"');
-      } else {
-        newStr.push(stringifyJSON(oKobj[i]));
       }
-    }
-    newStr.push("}");
-    // newStr2 = ["["];
-    // newStr2.push(newStr);
-    // newStr2.push("]");
-    return newStr.join("");
-  }
 
-};
+      newStr.push("}");
+      return newStr.join("");
+    }
+  }
+    if (obj === null) {
+      return '' + obj;
+    }
+    if (typeof obj === 'string') {
+      return '\"' + obj + '\"';
+    }
+    if (typeof obj === 'number') {
+      return '' + obj;
+    }
+    if (typeof obj === 'boolean') {
+      return '' + obj;
+    }
+    if (Array.isArray(obj) === true && obj.length !== 0) {
+      var newStr = [];
+      for (var i = 0; i < obj.length; i++) {
+        if (typeof obj[i] === 'number') {
+          newStr.push(obj[i]);
+        } else {
+          newStr.push(stringifyJSON(obj[i]));
+        }
+      }
+      newStr2 = ["["];
+      newStr2.push(newStr);
+      newStr2.push("]");
+      return newStr2.join("");
+    }
+    if (Array.isArray(obj) === true && obj.length === 0) {
+      var newStr = ["[", "]"];
+      return newStr.join("");
+    }
+    if (typeof obj === 'object' && Object.keys(obj).length === 0) {
+      var newStr = ["{", "}"];
+      return newStr.join("");
+    }
+  };
