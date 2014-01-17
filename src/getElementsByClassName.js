f70acca5aaee90b333a165b53b8b4033f1001670
Alex Jacobs <lex.jacobs@gmail.com>
2014-01-16 23:23:49 -0800

    getElementsByClassName - committing to preserve some fertal ideas along the path. Doesn't work yet.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,7 +3,73 @@
 //   return document.getElementsByClassName(className);
 // };
 
-// But in stead we're going to implement it from scratch:
-var getElementsByClassName = function (className) {
-  // your code here
+// But instead we're going to implement it from scratch:
+//You should use document.body, element.childNodes, and element.classList
+
+var getElementsByClassName = function(className) {
+  var result = [];
+
+  var walkDom = function(node, target) {
+
+    for (var i = 0; i < node.childNodes.length; i++) {
+      if (node.childNodes[i].classList !== undefined) {
+        for (var j = 0; j < node.childNodes[i].classList.length; j++) {
+          if (node.childNodes[i].classList[j] === target) {
+            result.push (node.childNodes[i].parentNode ) ;
+          }
+        }
+      }
+    }
+    return result;
+  };
+
+  return walkDom(document.body, className);
+
+
 };
+
+// the version above is working just fine, but what the function calls
+// isn't recursable for this purpose.
+// like the function below, the recursive function works by calling a 
+// function ON a SET of data. below, it's the array of arrays of arrays.
+// above, the SET of data should be the .childNodes of whatever is called in.
+// that way, the .childNodes of the .childNodes can be recursively scanned
+// for the existance of the className.
+
+
+// var testList = [1,2,3,[1,2,[6,7,[11,12,13,14],8],3],4,5];
+// var results = [];
+// var recurser = function(set){
+//   for (var i = 0; i < set.length; i++) {
+//     if (Array.isArray(set[i])){
+//       results.push(set[i]);
+//       return recurser(set[i]);
+//     }
+//   }
+//   return results;
+// };
+// console.log(recurser(testList));
+
+// var getElementsByClassName = function(className) {
+//   var result = [];
+//   for (var i = 0; i < document.body.childNodes.length; i++) {
+//     if (document.body.childNodes[i].classList !== undefined) {
+//       var resultList = document.body.childNodes[i].classList;
+//       for (var j = 0; j < resultList.length; j++) {
+//         if (resultList[j] === className) {
+//           console.log(resultList[j]);
+//           result.push(resultList[j]);
+//           return(result);
+//         }
+//       }
+//     }
+//   }
+// };
+
+// var getElementsByClassName = function(className) {
+//   var result = [];
+//   var $expectedNodes = $(".targetClassName");
+//   result.push($expectedNodes);
+//   // console.log(result);
+//   console.log(_.toArray(result[0]));
+// };
